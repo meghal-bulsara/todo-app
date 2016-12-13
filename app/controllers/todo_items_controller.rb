d3c02bfb8fff95f8af1b4c50ae714d249663925f
meghal <meghalbulsara22@gmail.com>
2016-12-13 15:33:31 +0530

    Mark as Completed

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,12 +1,12 @@
 class TodoItemsController < ApplicationController
   before_action :set_todo_list
+  before_action :set_todo_items, except: [:create]
   def create
     @todo_item = @todo_list.todo_items.create(todo_item_params)
     redirect_to @todo_list
   end
 
   def destroy
-    @todo_item = @todo_list.todo_items.find(params[:id])
     if @todo_item.destroy
       flash[:error] = "Todo List Item Was Deleted"
     else
@@ -14,7 +14,18 @@ class TodoItemsController < ApplicationController
     end
     redirect_to @todo_list
   end
+
+  def complete
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to @todo_list, notice: "Todo Item Completed"
+  end
+
   private
+
+def set_todo_items
+    @todo_item = @todo_list.todo_items.find(params[:id])
+end
+
   def set_todo_list
     @todo_list = TodoList.find(params[:todo_list_id])
   end
